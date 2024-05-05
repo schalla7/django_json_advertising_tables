6d286adf55bd62eaa52a51748a165736b09dbf51
Schalk Sevenster <schalk.sevenster@voss-solutions.com>
2024-05-05 20:57:41 +0200

    Add sorting and filtering of columns

--- advertplatform/views.py
+++ advertplatform/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from .models import Seller
+from django.db.models import Q
 
 def sellers_list(request):
-    sellers = Seller.objects.all().order_by('date_first_added')
-    # Add filtering logic here if needed
-    return render(request, 'sellers_list.html', {'sellers': sellers})
+    # Filtering
+    query = request.GET.get('q', '')  # Assume a simple text search in 'name' or 'domain'
+    sellers = Seller.objects.filter(
+        Q(name__icontains=query) | Q(domain__icontains=query)
+    )
+
+    # Sorting
+    sort_by = request.GET.get('sort', 'date_first_added')  # Default sort is by 'date_first_added'
+    order = request.GET.get('order', 'asc')
+
+    if order == 'desc':
+        sort_by = '-' + sort_by
+    sellers = sellers.order_by(sort_by)
+
+    return render(request, 'sellers_list.html', {'sellers': sellers, 'query': query, 'sort_by': sort_by, 'order': order})
